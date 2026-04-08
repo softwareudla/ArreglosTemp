4ad3942159e0cba71ce6c2e9dd98c1d5046a14cb
carlos.guaita <carlos.guaita@udla.edu.ec>
2026-04-08 10:15:16 -0500

    inicio

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+   
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+   
+
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+
+    return 0;
+}
